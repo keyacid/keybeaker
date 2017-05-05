f812eba17de8a8c8e0d237c8a781b541b6a34dd1
yvbbrjdr <xyyqzy@hotmail.com>
2017-05-05 13:09:06 +0000

    align button to right

--- resources/views/public_key_index.blade.php
+++ resources/views/public_key_index.blade.php
@@ -6,10 +6,18 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Manage public keys
-                    <button class="btn btn-primary" onclick="javascript:location.href='{{ url('/publickey/create') }}';"\>
-                        Add a public key
-                    </button>
+                    <table style="width:100%;">
+                        <tbody>
+                            <tr>
+                                <td>Manage public keys</td>
+                                <td style="text-align: right;">
+                                    <button class="btn btn-primary" onclick="javascript:location.href='{{ url('/publickey/create') }}';">
+                                        Add a public key
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
 
                 <div class="panel-body">
