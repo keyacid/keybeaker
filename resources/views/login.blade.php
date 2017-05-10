50a2bee21dd2b405c7ec780b8956671e6ccb5a88
yvbbrjdr <xyyqzy@hotmail.com>
2017-05-10 01:17:33 +0000

    add a space after sign message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Sign this message</label>
+                            <label class="col-md-4 control-label">Sign this message </label>
 
                             <div class="col-md-6">
                                 <label class="control-label">{{ $nonce }}</label>
