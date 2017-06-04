80e95e8e276dcb4d6a7701971977b2dce8a7cc1b
yvbbrjdr <xyyqzy@hotmail.com>
2017-06-04 07:18:52 +0000

    main page fit for mobile

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,94 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.index')
 
-        <title>404 - {{ env('APP_NAME', 'Keybeaker') }}</title>
+@section('title')
+404 - {{ env('APP_NAME', 'Keybeaker') }}
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+<div class="content">
+    <div class="title m-b-md">
+        404 Not Found
+    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .bottom-right {
-                position: absolute;
-                right: 10px;
-                bottom: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .copyright {
-                font-size: 15px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    404 Not Found
-                </div>
-
-                <div class="links">
-                    <a href="javascript:window.history.back();">Back</a>
-                </div>
-            </div>
-
-            <div class="bottom-right copyright">
-                Copyright &copy; {{ date('Y') }} yvbbrjdr
-            </div>
-        </div>
-    </body>
-</html>
+    <div class="links">
+        <a href="javascript:window.history.back();">Back</a>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.index')
 
-        <title>{{ env('APP_NAME', 'Keybeaker') }}</title>
+@section('title')
+{{ env('APP_NAME', 'Keybeaker') }}
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+<div class="top-right links">
+    <a href="{{ url('/inbox') }}">Inbox</a>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+<div class="content">
+    <div class="title m-b-md">
+        {{ env('APP_NAME', 'Keybeaker') }}
+    </div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .bottom-right {
-                position: absolute;
-                right: 10px;
-                bottom: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .copyright {
-                font-size: 15px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="top-right links">
-                <a href="{{ url('/inbox') }}">Inbox</a>
-            </div>
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{ env('APP_NAME', 'Keybeaker') }}
-                </div>
-
-                <div class="links">
-                    <a href="https://github.com/keyacid/">GitHub</a>
-                    <a href="https://github.com/keyacid/keybeaker/blob/master/LICENSE">License</a>
-                </div>
-            </div>
-
-            <div class="bottom-right copyright">
-                Copyright &copy; {{ date('Y') }} yvbbrjdr
-            </div>
-        </div>
-    </body>
-</html>
+    <div class="links">
+        <a href="https://github.com/keyacid/">GitHub</a>
+        <a href="https://github.com/keyacid/keybeaker/blob/master/LICENSE">License</a>
+    </div>
+</div>
+@endsection
